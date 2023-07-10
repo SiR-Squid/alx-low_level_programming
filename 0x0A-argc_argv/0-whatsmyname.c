835be1e626bb231ee37571534fdd11185c9f2188
SiR-Squid <gisorehaggai2@gmail.com>
2023-07-10 14:29:00 -0700

    whatsmyname francis hehe

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line
+ * @argc: argument that counts argument input
+ * @argv: argument that stores the strings in an array of char* (strings)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		argv[0] = argv[1];
+	}
+	else
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
